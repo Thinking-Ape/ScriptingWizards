2e85ebe31907e34e3a1821b0a46dc323845fb90b
Sebastian <sgunzelmann@arcor.de>
2020-04-19 16:34:46 +0200

    - updated changelog

--- src/main/model/GameConstants.java
+++ src/main/model/GameConstants.java
@@ -11,7 +11,7 @@ import java.nio.file.Paths;
 import java.util.Random;
 
 public abstract class GameConstants {
-    public static final boolean DEBUG = true;
+    public static final boolean DEBUG = false;
     public static final String VERSION = "1.5c";
 
     public static final double SCREEN_WIDTH = Toolkit.getDefaultToolkit().getScreenSize().getWidth();
